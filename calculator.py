5620e182dd4feb72eeaaac5da42d98fc0086c28f
HIMABINDU-GUNAGANTI <himabindugunaganti@gmail.com>
2022-08-15 14:56:21 +0530

    code modified

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def add(a,b):
-    sum=a+b;
-    return sum;
+    sum=a+b
+    return sum
 def sub(a,b):
-    sum=a-b;
-    return sum;
+    sum=a-b
+    return sum
 def mul(a,b):
-    sum=a*b;
-    return sum;
+    sum=a*b
+    return sum
 def div(a,b):
-    sum=a/b;
-    return sum;
+    sum=a/b
+    return sum
 a=int(input("Enter A value "))
 b=int(input("Enter B value "))
 print("The sum is ",add(a,b))
